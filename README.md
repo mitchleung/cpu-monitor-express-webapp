9109a081d7da2fe3f1e6df5a646f9dc7cf5cf247
Mitch Leung <mitchleung.com@gmail.com>
2020-10-12 11:46:23 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# CPU Monitor Express Server
+
+This is a server patching the CPU temperature and usage to JSON API calling from webapp counterpart.
+
+## Project setup
+```
+npm install
+```
+
+### Customize configuration
+Read more on [Configuration Reference on System Information](https://systeminformation.io).
+
+Read more on [Expressjs Reference](http://expressjs.com).
+
+
+### Run at root folder
+```
+node index.js
+```
+Nothing to build
+
+
+### License
+MIT License &copy; Mitch Leung
---
+++ index.js
@@ -0,0 +1,47 @@
+var cors = require("cors");
+const express = require("express");
+const app = express();
+const port = 3000;
+const si = require("systeminformation");
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Reference to API ");
+});
+
+app.get("/api/all", (req, res) => {
+  valueObject = {
+    cpuTemperature: "*",
+    mem: "used, free, total",
+    currentLoad: "currentload",
+  };
+  si.get(valueObject)
+    .then((data) => {
+      console.log(data);
+      console.log("...");
+      const results = {
+        cpu: {
+          total: data.currentLoad.currentload,
+          temp: data.cpuTemperature.main,
+          cores: data.cpuTemperature.cores,
+          max: data.cpuTemperature.max,
+        },
+        mem: {
+          free: data.mem.free,
+          used: data.mem.used,
+          total: data.mem.total,
+        },
+      };
+      console.log(results);
+      console.log("...");
+      // res.send(JSON.stringify(results));
+      res.send(results);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send({ error: error });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
